4c2499b9a651b91cbcbb82c0209ebbbde8bdc4fa
AsherBearce <49050298+AsherBearce@users.noreply.github.com>
2019-06-02 12:56:58 -0600

    Added a project link and short descriptions.

--- js/data.js
+++ js/data.js
@@ -7,15 +7,15 @@ const topBarTabs = [{
 
 // this is just json data of all the projects that you can edit at any time
 const projects = {
-    "Asher Cool Project": {
-        "desc": `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-        "github": "https://github.com/AsherBearce/something",
+    "Windows Command Prompt Rasterizer": {
+        "desc": `This is a project that creates methods to rasterize 3D models in the windows command prompt in real time, written in C++. Supports .obj files and texture mapping.`,
+        "github": "https://github.com/AsherBearce/Console-Rendering",
         "date": "mm/dd/yyyy",
         "creator": "Asher Bearce",
-        "shortDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "shortDesc": "A software rasterizer written in C++ for the windows command prompt.",
         "longDesc": [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ",
+            "This is a small software rasterizer I wrote to work in the windows command prompt. No external libraries were used, other than some of the standard libraries for manipulating windows in C++. This rasterizer supports perspective correct texture mapping, .obj files vertex colors, and even small fragment-shader like programs.",
+            "A lot of this project consisted of finding ways around the technical limitations of the command prompt. One of those limitations being the fact that the command prompt uses only four bits for colors. This means that only around 16 different colors are available. To get around this limitation, I implemented Flyod-Steinberg dithering to help the output of the rasterization look smoother, and appear to have more color capabilities. The big downside with this is that it slows down the rasterization considerably.",
             "https://via.placeholder.com/468x600",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
             "https://via.placeholder.com/600x560",
